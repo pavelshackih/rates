c7c9cf9564fa0f7926d6c7897c370a4a8189044c
Pavel Shackih <pavel.shackih@gmail.com>
2020-02-25 04:08:21 +0300

    remove unused

--- app/src/main/java/io/pavel/shackih/rates/domain/result.kt
+++
@@ -1,8 +0,0 @@
-package io.pavel.shackih.rates.domain
-
-sealed class AppResult
-
-data class OkResult(val list: List<Rate>, val fromLocal: Boolean = false) : AppResult()
-
-data class ErrorResult(val reason: Throwable, val message: String = "") : AppResult()
-
